679fe0ac61ef8c81a29f6150d40e936c8cc94c18
Shivam Sharma <shivamsharma8228@gmail.com>
2025-02-21 16:38:47 +0530

    code auto assignment of auditing parameters in BaseModel

--- src/main/java/com/shivam/splitwise/models/BaseModel.java
+++ src/main/java/com/shivam/splitwise/models/BaseModel.java
@@ -1,22 +1,25 @@
 package com.shivam.splitwise.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @CreatedDate
     private Date createdAt;
+    @LastModifiedDate
     private Date updatedAt;
     private boolean deleted;
 }
